a75020b3784d9cdcf1d0b4f7eb1eb2e3c09a1b96
derleymad <wanderleymbf@gmail.com>
2023-02-28 22:06:18 -0300

    fixing bug, problem was an'-' with the string token

--- app/src/main/java/com/derleymad/docegelato/ui/login/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/derleymad/docegelato/ui/login/fragments/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -93,6 +94,7 @@ class LoginFragment : Fragment() {
                     //BUG in LOGIN to not newuser
                     findNavController().navLoginToNewLocation()
                 }else{
+                    Log.i("loginfragment","camehere else")
                     val intent = Intent(requireActivity(), MainActivity::class.java)
                     startActivity(intent)
                     requireActivity().finish()
